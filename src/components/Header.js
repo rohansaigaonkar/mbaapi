8f09ed5b0b0ec720d3514a2da53b1b7d5b9533a5
rohan <rohan.saigaonkar97@gmail.com>
2021-01-31 01:33:09 -0500

    initial commit - added header, and bottom nav component. Will sleep now

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = (props) => {
+    return (
+        <div>
+            <h1>MBA API - An open source api for standardized testing</h1>
+        </div>
+    )
+}
+
+export default Header;
